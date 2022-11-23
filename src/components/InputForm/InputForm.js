732f9a9716e8f9a1bf1edc87cb8dfac6bdcdad34
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-11-23 20:56:22 +0000

    Update InputForm.js

--- src/components/InputForm/InputForm.js
+++ src/components/InputForm/InputForm.js
@@ -1,42 +1,40 @@
 import { useAddContactMutation, useGetContactsQuery } from 'API/contactsAPI';
 // styled components
-import { InputItem, Button, Label, FormDecoration } from "./InputForm.styled";
+import { InputItem, Button, Label, FormDecoration } from './InputForm.styled';
 // other libs
 import { Formik, Form } from 'formik';
 
-export function InputForm () {
-    const [addContact] = useAddContactMutation();
-    const { data } = useGetContactsQuery();
-    const contacts = data ?? [];
+export function InputForm() {
+  const [addContact] = useAddContactMutation();
+  const { data } = useGetContactsQuery();
+  const contacts = data ?? [];
 
-    const onSubmit = (values, action) => {
-        const equalName = contacts.find(el => (el.name.toLowerCase() === values.name.toLowerCase()));
-        if (equalName) return alert(equalName.name + " is already in contacts");
-        addContact(values);
-        action.resetForm();
-    }
-    return (
+  const onSubmit = (values, action) => {
+    const equalName = contacts.find(
+      el => el.name.toLowerCase() === values.name.toLowerCase()
+    );
+    if (equalName) return alert(equalName.name + ' is already in contacts');
+    addContact(values);
+    action.resetForm();
+  };
+   return (
         <Formik initialValues={{ name: "", number: "" }} onSubmit={onSubmit}>
-            <Form>
-                <FormDecoration>
-                    <Label>Name
+            <Form><label>Name
             <InputItem
                 type="text"
                 name="name"
                 pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
                 title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
                 required
-                        />
-                    </Label>
-            <Label>Number<InputItem
+            /></label>
+            <label>Number<InputItem
                 type="tel"
                 name="number"
                 pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
                 title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
                 required
-                /></Label>
-                <Button type="submit">Add contact</Button>
-            </FormDecoration>
+                /></label>
+                <button type="submit">Add contact</button>
             </Form>
         </Formik>
     )
