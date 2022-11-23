7f128c603b4d3fb90c5deabbbb700c1e529cf91a
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-11-23 22:46:39 +0000

    Update ContactItem.styled.js

--- src/components/ContactItem/ContactItem.styled.js
+++ src/components/ContactItem/ContactItem.styled.js
@@ -1,5 +1,13 @@
 import styled from 'styled-components';
 
+export const ContactListBox = styled.ul`
+  border: 3px solid steelblue;
+  max-width: 1100px;
+  margin: 20px;
+  border-radius: 10px;
+  background-color: white;
+  color: dimgray;
+`;
 
 export const ListItem = styled.li`
   display: flex;
