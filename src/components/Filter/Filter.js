748ce1873d4e6ceab7d56f3c50b1125fb53864ee
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-11-24 16:03:57 +0000

    Update Filter.js

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,16 +1,17 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { setFilter } from  'redux/filterSlice';
+import { setFilter } from 'redux/filterSlice';
+import { FindContacts, FindContactsBox, FindInput} from './Filter.styled';
 
 export const Filter = () => {
   const dispatch = useDispatch();
   const { value: filter } = useSelector(state => state.filter);
   return (
-    <div>
-      <h3>Find contacts by name</h3>
-      <input
+    <FindContactsBox>
+      <FindContacts >Find contacts by name</FindContacts >
+      <FindInput
         type="text"
         value={filter}
         onChange={evt => dispatch(setFilter(evt.currentTarget.value))} />
-    </div>
+    </FindContactsBox>
   )
 }
