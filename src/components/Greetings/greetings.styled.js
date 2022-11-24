e274c8b6939b5856b7b14efd751525d60e1e0d79
Roman <97307962+RomanKozhushko@users.noreply.github.com>
2022-11-24 16:47:02 +0000

    Update greetings.styled.js

--- src/components/Greetings/greetings.styled.js
+++ src/components/Greetings/greetings.styled.js
@@ -2,7 +2,6 @@ import styled from 'styled-components';
 import image from 'img/file.jpeg';
 
 export const Home = styled.div`
-width: 1100px;
 padding: 20px;
 background-image: url(${ image });
 background-size: cover;
